4db0d49cdebc1767edb9ae20b230e2232601e44d
Shardul Vaje <Shardul Vaje@2317LAP.goapl.com>
2025-01-28 11:15:31 +0530

    Deploy v8 via git cli

--- app1/app.py
+++ app1/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Define a route for the root URL
 @app.route('/')
 def hello_world():
-    return 'Hello, Shardul from App 1 v7.1!'
+    return 'Hello, Shardul from Develop App 1 v8!'
 
 # Run the Flask application if this file is executed directly
 if __name__ == '__main__':
